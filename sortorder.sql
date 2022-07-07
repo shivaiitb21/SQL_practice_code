675b08e3328e0095b93c0c8daf0194b162d1f741
shivaiitb21 <shivnalgire4@gmail.com>
2022-07-07 23:03:41 +0530

    sort by multiple columns

---
+++ sortorder.sql
@@ -0,0 +1,5 @@
+-- The following SQL statement selects all customers from the "Customers" table, 
+-- sorted ascending by the "Country" and descending by the "CustomerName" column:
+
+SELECT * FROM Customers
+ORDER BY Country ASC, CustomerName DESC;
