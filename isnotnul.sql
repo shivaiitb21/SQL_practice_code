ad61118033e7378088d2448cdee24329653a74f8
shivaiitb21 <shivnalgire4@gmail.com>
2022-07-08 20:09:42 +0530

    is not null

---
+++ isnotnul.sql
@@ -0,0 +1,7 @@
+-- The IS NOT NULL operator is used to test for non-empty values (NOT NULL values).
+
+-- The following SQL lists all customers with a value in the "Address" field:
+
+SELECT CustomerName, ContactName, Address
+FROM Customers
+WHERE Address IS NOT NULL;
