b6906135f8f012fc4966adc98a7966a9d3f51399
shivaiitb21 <shivnalgire4@gmail.com>
2022-07-08 20:04:03 +0530

    Added example

--- insrt.sql
+++ insrt.sql
@@ -2,3 +2,6 @@
 
 INSERT INTO Customers (CustomerName, ContactName, Address, City, PostalCode, Country)
 VALUES ('Cardinal','Tom B. Erichsen','Skagen 21','Stavanger','4006','Norway');
+
+INSERT INTO Customers (CustomerName, City, Country)
+VALUES ('Cardinal', 'Stavanger', 'Norway');
