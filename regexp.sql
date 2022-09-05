7b53343c30866ece79ec90bff86b93a6b4bbd5a3
shivaiitb21 <shivnalgire4@gmail.com>
2022-09-05 19:12:31 +0530

    regular expression starting with a,e,i,o,u

---
+++ regexp.sql
@@ -0,0 +1,4 @@
+-- Query the list of CITY names starting with vowels (i.e., a, e, i, o, or u) from STATION. Your result cannot contain duplicates.
+
+SELECT DISTINCT CITY FROM STATION
+WHERE CITY REGEXP '^[a,e,i,o,u]' ORDER BY CITY;
