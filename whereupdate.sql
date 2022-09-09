0aeb9899de922eda7448fe425aad0412b39035a5
shivaiitb21 <shivnalgire4@gmail.com>
2022-09-09 10:37:47 +0530

    where clause determines how many contacts are updated

--- whereupdate.sql
+++ whereupdate.sql
@@ -1,2 +1,7 @@
 -- It is the WHERE clause that determines how many records will be updated
 -- WHERE Satetement determines how many record will be updated
+
+-- following SQL statement will update the ContactName to "Juan" for all records where country is "Mexico":
+UPDATE Customers
+SET ContactName='Juan'
+WHERE Country='Mexico';
