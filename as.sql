dfa796e6ca089fbed4dcd137a2aaf935153a0837
shivaiitb21 <shivnalgire4@gmail.com>
2022-09-10 09:50:18 +0530

    as statement

---
+++ as.sql
@@ -0,0 +1,10 @@
+-- The AS command is used to rename a column or table with an alias.
+
+-- An alias only exists for the duration of the query.
+
+SELECT CustomerID AS ID, CustomerName AS Customer
+FROM Customers;
+
+-- The following SQL statement creates an alias named "Address" that combine four columns (Address, PostalCode, City and Country):
+SELECT CustomerName, Address + ', ' + PostalCode + ' ' + City + ', ' + Country AS Address
+FROM Customers;
