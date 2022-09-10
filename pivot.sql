15b6fb8825fb60734c6669265d89a0bee391a691
Shivanand Nalgire <62739431+shivaiitb21@users.noreply.github.com>
2022-09-10 13:20:25 +0530

    pivot problem and link

--- pivot.sql
+++ pivot.sql
@@ -1,6 +1,7 @@
 --Pivot the Occupation column in OCCUPATIONS so that each Name is sorted alphabetically and displayed underneath its corresponding Occupation. 
 --The output column headers should be Doctor, Professor, Singer, and Actor, respectively.
 --Note: Print NULL when there are no more names corresponding to an occupation.
+-- https://www.hackerrank.com/challenges/occupations/problem?isFullScreen=true&h_r=next-challenge&h_v=zen&h_r=next-challenge&h_v=zen
 
 SELECT 
 MAX(CASE WHEN Occupation = 'Doctor' then Name END), 
